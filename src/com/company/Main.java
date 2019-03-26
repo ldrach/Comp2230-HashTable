71cd1a61225cb7818d2df41ea2e654d91c3508a3
Lyndon <lyndondrach@gmail.com>
2019-03-26 07:38:44 -0700

    cleaned up code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,32 @@
 package com.company;
 
-
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
-       Random rand = new Random(13);
+        // Initializes Random Number and Arrays
+        Random rand = new Random(13);
+        HashTable table = new HashTable();
 
-       int[][] hashTable = new int[50][5];
-       int[] overFlow = new int[50];
-
-
-       for(int i=0; i<200;i++){
-           int num = rand.nextInt(9998)+1;
-           int index=num%50;
-           int index2=0;
-
-           
-           hashTable[index][index2] = num;
-       }
-
-        for(int i=0;i<50;i++){
-            for(int j=0; j<5;j++){
-               System.out.print(hashTable[i][j] + " ");
-            }
-            System.out.println();
+        for (int i = 0; i < 200; i++) {
+            int num = rand.nextInt(9998) + 1;
+            table.add(num);
         }
+
+        table.showArray();
+        //removes the first number in a full row
+        table.remove(3820);
+        //removes the only number from a row
+        table.remove(1334);
+        //removes number from overflow
+        table.remove(50);
+        //removes a number added to hashtable from overflow
+        table.remove(6071);
+        //removes a number not in the overflow or hashtable
+        table.remove(1000000);
+
+        table.showArray();
     }
 }
